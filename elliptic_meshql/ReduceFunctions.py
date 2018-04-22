c05d50c79008f695bc40254aa485f488f797c197
Guilherme Caminha <gpkc@cin.ufpe.br>
2018-04-21 22:27:48 -0300

    Fixing reduce_sum name.

--- elliptic_meshql/ReduceFunctions.py
+++ elliptic_meshql/ReduceFunctions.py
@@ -4,4 +4,4 @@ from .Computer import MeshQLFunction
 class Sum(MeshQLFunction):
 
     def __init__(self, initial_value):
-        super().__init__('initial_value', builtin=True, initial_value=initial_value)
+        super().__init__('reduce_sum', builtin=True, initial_value=initial_value)
