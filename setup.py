f11447108ec298351a9334ef786ec6a073f38e2e
Guilherme Caminha <gpkc@cin.ufpe.br>
2018-04-21 22:24:35 -0300

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/padmec-reservoir/elliptic_meshql',
-    version='0.2.2',
+    version='0.2.3',
     zip_safe=False,
 )
